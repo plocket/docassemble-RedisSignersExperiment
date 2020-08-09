7d0ba96c3b98169c5a896813567bd9e00af561f4
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-08-09 14:24:21 -0400

    Start moving abstractions. `willing_to_sign` not found.

--- docassemble/RedisSignersExperiment/signing_party.py
+++ docassemble/RedisSignersExperiment/signing_party.py
@@ -1,18 +1,19 @@
-from docassemble.base.util import DARedis, DAObject
+from docassemble.base.util import DARedis, Individual, log #, DAObject
 
 # Use and save data in redis if possible
 #class SigningParty(DAObject):
 class SigningParty():
   # Only uses redis if url arguments exist to allow this module to be used independently of url arguments. Somewhere else will take care of authentication?
   #def init(self, url_args):
-  def __init__(self, url_args):
+  def __init__(self, url_args, obj):
     #super().init(*pargs, **kwargs)
     self.exists = False
-    
+    log( 'a', 'console' )
     # In url_args there should be:
     # action_key, user_id, party_id
 
     if 'action_key' in url_args:
+      log( 'b', 'console' )
       self.exists = True
       self.redis = DARedis()
       redis = self.redis
@@ -31,12 +32,53 @@ class SigningParty():
       self.signature = valueOrNone( party, 'signature' )
       self.signature_date = valueOrNone( party, 'signature_date' )
       self.willing_to_sign = valueOrNone( party, 'willing_to_sign' )
+      self.has_signed = valueOrNone( party, 'has_signed' )
+
+      #log( 'willing_to_sign', 'console' )
+      #log( valueOrNone( party, 'willing_to_sign' ), 'console' )
+      #self.amend( 'willing_to_sign', valueOrNone( party, 'willing_to_sign' ))
+      #log( self.willing_to_sign, 'console' )
 
   def amend(self, key, value):
     if self.exists:
+      log( 'module:', 'console' )
+      log( self.action_data['parties'][ self.id ], 'console' )
+      log( key, 'console' )
+      log( value, 'console' )
       setattr( self.action_data['parties'][ self.id ], key, value ) #.setAttribute( key, value )
       self.redis.set_data( self.action_key, self.action_data )
 
 def valueOrNone( dictionary, key ):
-  try: return dictionary[ key ]
+  try:
+    log( 'valueOrNone', 'console' )
+    log( repr(dictionary), 'console' )
+    return dictionary[ key ]
   except: return None
+
+
+redis = DARedis()
+  
+def amend_signer( url_args, key, value ):
+  log( 'amend:', 'console' )
+  log( key, 'console' )
+  log( value, 'console' )
+
+  if 'action_key' in url_args:
+    party_id = url_args['party_id']
+    action_data = redis.get_data( url_args[ 'action_key' ] )
+    log( action_data['parties'][ party_id ], 'console' )  # party
+
+    # set party attribute
+    setattr( action_data['parties'][ party_id ], key, value )
+    redis.set_data( url_args[ 'action_key' ], action_data )
+    return action_data['parties'][ party_id ]
+  else:
+    return None
+  
+def get_signer( url_args ):
+  if 'action_key' in url_args:
+    party_id = url_args['party_id']
+    return redis.get_data( url_args[ 'action_key' ] )['parties'][ party_id ]
+  else:
+    return None
+
