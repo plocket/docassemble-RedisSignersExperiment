d3045d4db8b7c25a4ec974488b8063b88ed4acbf
Anonymous User <52798256+plocket@users.noreply.github.com>
2020-08-12 09:07:58 -0400

    Begin transition to try sign_on_device without a generic object

--- docassemble/RedisSignersExperiment/signing_party.py
+++ docassemble/RedisSignersExperiment/signing_party.py
@@ -2,23 +2,35 @@ from docassemble.base.util import log, DARedis#, Individual, #, DAObject
 
 redis = DARedis()
   
-def amend_signer( url_args, key, value ):
-  if 'action_key' in url_args:
-    party_id = url_args['party_id']
-    action_data = redis.get_data( url_args['action_key'] )
+def amend_signer( data, key, value ):
+  if 'action_key' in data:
+    party_id = data['party_id']
+    action_data = redis.get_data( data['action_key'] )
 
     # set party key
     action_data['parties'][ party_id ][ key ] = value
-    redis.set_data( url_args[ 'action_key' ], action_data )
+    redis.set_data( data[ 'action_key' ], action_data )
     return action_data['parties'][ party_id ]
   else:
     return None
   
-def get_signer( url_args ):
-  if 'action_key' in url_args:
-    party_id = url_args['party_id']
-    if party_id in redis.get_data( url_args['action_key'] )['parties']:
-      return redis.get_data( url_args['action_key'] )['parties'][ party_id ]
+def get_signer( data ):
+  
+  #if 'action_key' in url_args:
+  #  data = url_args
+  #
+  ## Will this error?
+  #elif action_argument('action_key'):
+  #  data = {
+  #    #'parent_interview_data_id': action_argument('parent_interview_data_id'),
+  #    'action_key': action_argument('action_key'),
+  #    'party_id': action_argument('party_id')
+  #  }
+
+  if 'action_key' in data:
+    party_id = data['party_id']
+    if party_id in redis.get_data( data['action_key'] )['parties']:
+      return redis.get_data( data['action_key'] )['parties'][ party_id ]
     else:
       return False
   else:
